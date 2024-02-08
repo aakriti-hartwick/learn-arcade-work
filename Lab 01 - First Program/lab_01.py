7986e010bb8316f8bc49b8c2ee955067d9c7ab23
aakriti-hartwick <chaudharya@hartwick.edu>
2024-02-08 09:57:35 -0500

    homework

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,9 +1,8 @@
 print("Hello World")
-print('proramming in python is intrestig\n\nbarbie is fantastic animated movies to watch')
+print('programming in python is interesting\n\nbarbie is fantastic animated movies to watch')
 print()
 print('how are you?\t create a beautiful place to live on earth by spreading love and care')
-print('nothing in world is impossible you have to tey and focus to be on your way\t and be happy and creative for healthy life')
+print('nothing in world is impossible you have to tey and focus to be on your way\t  be happy for healthy life')
 print()
-
-print('commitment is the best key to success in our life \ndonot let anyone put you down and get up whenever you are feeling low ')
-print('fashion is  one of the most remarkable  best carrer for the new generation who aims to be creative and powerful in the field of designing')
+print('commitment is the best key to success in our life \ndo not let anyone put you down and get up whenever you are feeling low ')
+print('fashion is most remarkable career for generation who aims to be creative  in the field of art')
