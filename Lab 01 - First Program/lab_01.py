22c42ab0615e517db5b8915d2a5c27b0fb80c067
aakriti-hartwick <chaudharya@hartwick.edu>
2024-02-08 09:49:47 -0500

    homework
    barbie

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,9 @@
-hello wolrd nice to meet you all
+print("Hello World")
+print('proramming in python is intrestig\n\nbarbie is fantastic animated movies to watch')
+print()
+print('how are you?\t create a beautiful place to live on earth by spreading love and care')
+print('nothing in world is impossible you have to tey and focus to be on your way\t and be happy and creative for healthy life')
+print()
+
+print('commitment is the best key to success in our life \ndonot let anyone put you down and get up whenever you are feeling low ')
+print('fashion is  one of the most remarkable  best carrer for the new generation who aims to be creative and powerful in the field of designing')
--- Testing/test.py
+++
@@ -1,4 +0,0 @@
-hello world,how are you all
-i love myself
-nice to meet you all
-hope you all are doing great
