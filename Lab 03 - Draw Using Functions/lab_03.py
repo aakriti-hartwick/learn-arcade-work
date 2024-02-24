f7b7cf8cb0800efc8abe04d4758e6fd57edf645c
aakriti-hartwick <chaudharya@hartwick.edu>
2024-02-24 12:18:46 -0500

    fff

---
+++ Lab 03 - Draw Using Functions/lab_03.py
@@ -0,0 +1,21 @@
+"""
+This is my lab 3 project.
+"""
+# Concentration is the most powerful thing.
+
+
+import arcade
+
+screen_width = 750
+
+screen_height = 900
+
+def main():
+    arcade.open_window(750 , 900)
+    arcade.set_background_color(arcade.color.HARLEQUIN)
+    arcade.start_render()
+
+
+
+
+
