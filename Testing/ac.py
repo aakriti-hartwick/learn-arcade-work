d6079cf4472bb48e686d48597b2355c384c81a0e
aakriti-hartwick <chaudharya@hartwick.edu>
2024-03-11 08:56:48 -0400

    Merge remote-tracking branch 'origin/master'

--- Testing/ac.py
+++ Testing/ac.py
@@ -1,18 +1,29 @@
 import random
 
+# Aakriti Chaudhary Camel game
+# Welcome to my world where I programmed a textual game Camel
+
+# Using main function
 def main():
 
-    print('Welcome to camel')
+# What we are going to do in the game
+
+    print('\nWelcome to camel')
     print("You have stolen camel to make your way across the Mobi desert")
-    print("The natives want their camel back and are chasing you down! Survive your")
-    print("desert trek and out run the natives")
+    print("The natives want their camel back and are chasing you down!")
+    print("Survive your desert trek and out run the natives.\n")
+
+# To watch things
 
     done = False
+
     miles_travelled = 0
     thirst = 0
     camel_tiredness = 0
     natives_distance = -20
     water_left = 3
+
+# player options to choose from
 
     while not done:
 
@@ -26,21 +37,30 @@ def main():
         user_choice = input("What is your choice? ")
 
         if user_choice.upper() == 'F':
+
             print("Game end.")
             break
 
         elif user_choice.upper() =='E':
-            print("\nDistance Travelled:" +str(miles_travelled))
+
+            print("\nDistance Travelled: " + str(miles_travelled) + " miles")
             print("You have " + str(water_left) + " drinks left.")
+            print("Camel tiredness: " + str(camel_tiredness))
+            print("Thirst level: " + str(thirst))
             print("The natives are " +str(miles_travelled - natives_distance) + " miles behind you.\n")
 
+
         elif user_choice.upper() == "D":
+
             print("\nYou slept for the whole night.")
             print("Your camel is satisfied.")
+            camel_tiredness = 0
             natives_distance= natives_distance + random.randrange(7,14)
             print("The natives are " + str(miles_travelled - natives_distance) + " miles behind you.\n")
 
+
         elif user_choice.upper() == "C":
+
             thirst= thirst + 1
             print("\nYour camel ran in full speed.")
             miles_travelled = miles_travelled + random.randrange(10,20)
@@ -48,7 +68,18 @@ def main():
             natives_distance= natives_distance + random.randrange(7,14)
             print("The natives are " + str(miles_travelled - natives_distance) + " miles behind you.\n")
 
+            if random.randint(1, 20) == 1:
+                print("You found an Oasis. You drank water, took rest and filled your canteen.\n")
+                camel_tiredness = 0
+                water_left = 3
+                thirst = 0
+
+            else:
+                pass
+
+
         elif user_choice.upper() == "B":
+
             thirst= thirst + 1
             print("\nYour camel ran in medium speed.")
             miles_travelled = miles_travelled + random.randrange(5,12)
@@ -56,13 +87,26 @@ def main():
             natives_distance= natives_distance + random.randrange(7,14)
             print("The natives are " + str(miles_travelled - natives_distance) + " miles behind you.\n")
 
+            if random.randint(1, 20) == 1:
+                print("You found an Oasis. You drank water, took rest and filled your canteen.\n")
+                camel_tiredness = 0
+                water_left = 3
+                thirst = 0
+
+            else:
+                pass
+
+
         elif user_choice.upper() == "A":
+
             if water_left > 0:
                 thirst = 0
                 water_left = water_left - 1
                 print("\nWow! That hit the spot.\n")
             else:
                 print("\nYou ran out of water.\n")
+
+# Calculation and result of stats
 
         if thirst > 6:
             print("Your camel died.")
@@ -71,28 +115,23 @@ def main():
         elif thirst > 4:
             print("Your camel is thirsty. Drink water!\n")
 
-            if camel_tiredness > 8:
-                print("Your camel died . He was tired . \n")
-                done = True
-                break
+        if camel_tiredness > 8:
+            print("Your camel died. He was tired.\n")
+            done = True
+            break
+        elif camel_tiredness > 5:
+            print("Your camel is getting tired. Take rest.\n")
 
-            elif camel_tiredness > 5 :
-                print("Camel is getting tired . Take rest .\n")
+        if natives_distance >= miles_travelled:
+            print("The natives caught you. Better luck next time.\n")
+            done = True
+            break
+        elif miles_travelled - natives_distance <= 15:
+            print("Run faster!! The natives are getting closer.\n")
 
-                if natives_distance >= miles_travelled:
-                    print("The natives caught you. Better luck next time.\n")
-                    done = True
-                    break
-                elif miles_travelled - natives_distance <= 15:
-                    print("Run faster!! The natives are getting closer.\n")
-
-                if miles_travelled >= 200:
-                    print("You crossed the desert. You won.\n")
-                    done = True
-                    break
-
-
-
-
+        if miles_travelled >= 200:
+            print("You crossed the desert. You won.\n")
+            done = True
+            break
 
 main()
--- Testing/p.py
+++ Testing/p.py
@@ -4,20 +4,13 @@ import arcade
 def draw_section_outlines():
     # Draw squares on bottom
     arcade.draw_rectangle_outline(150, 150, 300, 300, arcade.color.BLACK)
-    arcade.draw_rectangle_outline(450, 150, 300, 300, arcade.color.BLACK)
-    arcade.draw_rectangle_outline(750, 150, 300, 300, arcade.color.BLACK)
-    arcade.draw_rectangle_outline(1050, 150, 300, 300, arcade.color.BLACK)
-
-    # Draw squares on top
-    arcade.draw_rectangle_outline(150, 450, 300, 300, arcade.color.BLACK)
-    arcade.draw_rectangle_outline(450, 450, 300, 300, arcade.color.BLACK)
-    arcade.draw_rectangle_outline(750, 450, 300, 300, arcade.color.BLACK)
-    arcade.draw_rectangle_outline(1050, 450, 300, 300, arcade.color.BLACK)
-
-
+@@ -16,49 +18,40 @@ def draw_section_outlines():
 def draw_section_1():
     for row in range(30):
         for column in range(30):
+            x = (column*10) + 5# Instead of zero, calculate the proper x location using 'column'
+            y =(row *10) + 5 # Instead of zero, calculate the proper y location using 'row'
+            arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
 
             x = (column * 10) + 5
             y = (row * 10) + 5
@@ -25,8 +18,14 @@ def draw_section_1():
 
 
 def draw_section_2():
+    # Below, replace "pass" with your code for the loop.
+    # Use the modulus operator and an if statement to select the color
+    # Don't loop from 30 to 60 to shift everything over, just add 300 to x.
+
     for row in range(30):
         for column in range(30):
+            x = (column * 10 )+ 305
+            y = (row * 10 )+ 5
             x = (column * 10) + 305
             y = (row * 10) + 5
             if column % 2 == 0:
@@ -35,31 +34,43 @@ def draw_section_2():
                 arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.BLACK)
 
 
+
 def draw_section_3():
+    # Use the modulus operator and an if/else statement to select the color.
+    # Don't use multiple 'if' statements.
     for row in range(30):
+        for column in range (30):
+            x = (column * 10) +605
         for column in range(30):
             x = (column * 10) + 605
             y = (row * 10) + 5
             if row % 2 == 0:
+                arcade.draw_rectangle_filled(x , y, 5, 5, arcade.color.WHITE)
                 arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
             else:
+                arcade.draw_rectangle_filled(x, y, 5, 5 , arcade.color.BLACK)
+
                 arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.BLACK)
 
 
 def draw_section_4():
+    # Use the modulus operator and just one 'if' statement to select the color.
     for row in range(30):
         for column in range(30):
             x = (column * 10) + 905
-            y = 295- (row * 10)
+            y = (row * 10) + 5
+            if (column + row)% 2  and column % 2  == 0:
             if (column + row) % 2 and column % 2 == 0:
 
                 arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
             else:
-                arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.BLACK)
-
+@@ -67,15 +60,14 @@ def draw_section_4():
 
 def draw_section_5():
     # Do NOT use 'if' statements to complete 5-8. Manipulate the loops instead.
+    for row in range(30):
+        for column in range(row - 30):
+            x = (column * 10) +5
     for row in range(31):
         for column in range(row + 1, 31):
             x = (column * 10) - 5
@@ -68,24 +79,30 @@ def draw_section_5():
             arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
 
 
+
 def draw_section_6():
     for row in range(30):
         for column in range(30 - row):
-            x = (column * 10) + 305
-            y = (row * 10) + 305
-
-            arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
-
+@@ -87,25 +79,22 @@ def draw_section_6():
 
 def draw_section_7():
     for row in range(30):
+        for column in range(row + 1 ):
         for column in range(row + 1):
             x = (column * 10) + 605
             y = (row * 10) + 305
             arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
 
 
+
 def draw_section_8():
+    for row in range(300):
+        for column in range(150 * row ):
+
+           x = (column * 10) + 905
+           y = (row * 10) + 305
+           arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
+
     for row in range(30):
         for column in range(30 - row, 48):
             x = (column * 10) + 895
@@ -94,27 +111,12 @@ def draw_section_8():
 
 
 def main():
+    # Create a window
 
     arcade.open_window(1200, 600, "Lab 05 - Loopy Lab")
     arcade.set_background_color(arcade.color.AIR_FORCE_BLUE)
 
-    arcade.start_render()
-
-    # Draw the outlines for the sections
-    draw_section_outlines()
-
-    # Draw the sections
-    draw_section_1()
-    draw_section_2()
-    draw_section_3()
-    draw_section_4()
-    draw_section_5()
-    draw_section_6()
-    draw_section_7()
-    draw_section_8()
-
-    arcade.finish_render()
-
+@@ -129,4 +118,4 @@ def main():
     arcade.run()
 
 
