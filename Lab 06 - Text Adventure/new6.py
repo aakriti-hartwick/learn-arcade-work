78ce8f4a42a78389d0b39191d7c93bbcc4f3af26
aakriti-hartwick <chaudharya@hartwick.edu>
2024-03-26 17:18:05 -0400

    Merge remote-tracking branch 'origin/master'

---
+++ Lab 06 - Text Adventure/new6.py
@@ -0,0 +1,24 @@
+class Room:
+    """
+    This is a class that represents the player character.
+    """
+    def __init__(self, description, north, south, east, west):
+        """ This is a method that sets up the variables in the object. """
+        self.description = description
+        self.north = north
+        self.south = south
+        self.east = east
+        self.west = west
+
+
+# Create an empty list
+    my_list = {}
+
+    # Creating a variables for room named my_list
+
+
+my_list = [0, 1, 2, 3, 4]
+# Rooms description
+room_description = "You enter to master bedroom which has king size bed and large window."
+
+
